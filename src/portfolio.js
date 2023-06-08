cd50762761a2e5d1c96a49bb537e8797973e5e93
Abinash Bordoloi <75088946+Abinash-bordoloi07@users.noreply.github.com>
2023-06-08 18:09:16 +0530

    greeting editited at portfolio.json

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,21 +7,21 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Abinash's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Abinash Bordoloi Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "http://abinashbordoloi.com/",
   },
 };
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Abinash Bordoloi",
+  logo_name: "Abinash",
+  nickname: "I don't have one",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
